bdfe951076fb529eb2247b191736954268cce57e
Matteo  Santoro <sherl@MacBook-Pro-di-Matteo.local>
2020-03-03 20:33:37 +0100

    removed useless events startup

--- app/main.py
+++ app/main.py
@@ -1,19 +1,13 @@
 
 from app.api.routes.api import router as api_router
 from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException
-
-from app.core.events import create_start_app_handler
 
 
 def get_application() -> FastAPI:
 
     application = FastAPI(title="Songure")
     application.include_router(api_router)
-
-    application.add_event_handler("startup", create_start_app_handler(application))
-
     return application
 
 
