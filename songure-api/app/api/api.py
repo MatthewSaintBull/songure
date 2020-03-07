1e86b48fd145b4e2a8b0324fc8f179b3d9dd410d
Matteo Santoro <m.santoro1997@gmail.com>
2020-03-07 13:43:59 +0100

    changed prefix api

--- songure-api/app/api/api.py
+++ songure-api/app/api/api.py
@@ -4,5 +4,5 @@ from app.api.routes import register, login
 
 
 router = APIRouter()
-router.include_router(register.router, tags=["register"], prefix="/v1")
 router.include_router(login.router, tags=["login"], )
+router.include_router(register.router, tags=["register"], prefix="/api")
